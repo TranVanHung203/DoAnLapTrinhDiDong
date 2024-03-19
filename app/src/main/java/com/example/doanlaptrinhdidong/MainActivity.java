3ac083b5577705f2749def7edd58fef45f6e2017
Khang <khangatvttt@gmail.com>
2024-03-19 22:50:25 +0700

    Up lại nhe

--- app/src/main/java/com/example/doanlaptrinhdidong/MainActivity.java
+++ app/src/main/java/com/example/doanlaptrinhdidong/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Vạn sự khởi đầu nan
+        //Up lần nữa
     }
 }
