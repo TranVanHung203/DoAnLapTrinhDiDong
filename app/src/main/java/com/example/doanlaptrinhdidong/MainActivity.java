967580c18911dbda4f4aa76198ff1ab4783c9e90
nhatkhanh <nguyennhatkhanh12102019@gmail.com>
2024-03-19 22:50:01 +0700

    tui mới úp thử bằng android nè

--- app/src/main/java/com/example/doanlaptrinhdidong/MainActivity.java
+++ app/src/main/java/com/example/doanlaptrinhdidong/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Gian nan đựng có nản
+        //tui up thu bang android ne
     }
 }
